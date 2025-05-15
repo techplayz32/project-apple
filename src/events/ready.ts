728463d43491b9edde41ea2ba676a944c4618923
Tech <154914363+techplayz32@users.noreply.github.com>
2025-05-15 17:45:56 +0300

    Delete src directory, redoing everything..

--- src/events/ready.ts
+++
@@ -1,9 +0,0 @@
-const { Events } = require('discord.js');
-
-module.exports = {
-	name: Events.ClientReady,
-	once: true,
-	execute(client: { user: { tag: any; }; }) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
-};
